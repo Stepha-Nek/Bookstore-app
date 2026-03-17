2172ccd53143dd695dbb013e9e384891e1fae681
Nneka Oguh <nneka.oguh1@gmail.com>
2026-03-17 20:56:55 +0000

    fix controller tests - add JWT config for test environment

--- firstAPI.Tests/Helpers/TestWebAppFactory.cs
+++ firstAPI.Tests/Helpers/TestWebAppFactory.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 
 namespace firstAPI.Tests.Helpers
 {
@@ -10,6 +12,17 @@ namespace firstAPI.Tests.Helpers
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
+            // Add JWT config for testing
+            builder.ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddInMemoryCollection(new Dictionary<string, string?>
+                {
+                    ["Jwt:Key"] = "TestSuperSecretKeyThatIsAtLeast32CharactersLong!",
+                    ["Jwt:Issuer"] = "FirstAPI",
+                    ["Jwt:Audience"] = "FirstAPIUsers"
+                });
+            });
+
             builder.ConfigureServices(services =>
             {
                 // Remove everything DB related
@@ -33,3 +46,4 @@ namespace firstAPI.Tests.Helpers
         }
     }
 }
+
